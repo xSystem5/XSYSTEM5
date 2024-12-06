4c8dd070309dbfe8261dfea69d1cd73296aaf307
xSystem5 <xsyetem5@gmail.com>
2024-12-06 15:07:08 -0600

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -64,3 +64,16 @@ window.addEventListener('DOMContentLoaded', () => {
   initParticles();
   animateParticles();
 });
+
+// Menú hamburguesa
+const menuToggle = document.getElementById('menuToggle');
+const mobileMenu = document.getElementById('mobileMenu');
+
+menuToggle.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+  gsap.fromTo(
+    mobileMenu,
+    { opacity: 0, y: -20 },
+    { opacity: 1, y: 0, duration: 0.5, ease: 'power2.out' }
+  );
+});
